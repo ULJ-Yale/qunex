559fee9317b444672d0c126b418b395721d91fcb
Ben Acland <devnull@localhost>
2014-02-13 13:12:56 -0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='g_mri',
+      version='1.0',
+      author='Grega Repovs',
+      description='',
+      author_email='',
+      url='https://bitbucket.org/mblab/g_mri',
+      package_dir={'':'../'},
+      packages=['g_mri'],
+      scripts=['gmri'],
+      )
