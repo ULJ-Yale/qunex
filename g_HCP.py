17f1409557f23ef4cdd2caf2a5d6cf5456e9220c
Grega Repovs <grega.repovs@psy.ff.uni-lj.si>
2014-07-26 21:20:00 +0200

    Adjusted setupHCP to map all present SpinEchoFieldMaps. It adds the number of the bold for which it was run to folder name.

--- g_HCP.py
+++ g_HCP.py
@@ -13,7 +13,7 @@ def setupHCP(folder=".", tfolder="hcp", sbjf="subject.txt"):
     rawf     = inf['raw_data']
     sid      = inf['id']
     bolds    = collections.defaultdict(dict)
-    
+
     if not os.path.exists(rawf):
         print "ERROR: raw_data folder for %s does not exist! Check your paths [%s]! Aborting setupHCP.\n" % (sid, rawf)
         return
@@ -26,74 +26,77 @@ def setupHCP(folder=".", tfolder="hcp", sbjf="subject.txt"):
     else:
         print " ...  Base folder %s already exists " % (basef)
 
-    for k, v in inf.iteritems():
-        if k.isdigit():
-            if v['name'] == 'T1w':
-                sfile = k+"-o.nii.gz"
-                tfile = sid + "_strc_T1w_MPR1.nii.gz"
-                tfold = "T1w"
-            elif v['name'] == "T2w":
-                sfile = k+"-o.nii.gz"
-                tfile = sid + "_strc_T2w_SPC1.nii.gz"
-                tfold = "T2w"
-            elif v['name'] == "FM-Magnitude":
-                sfile = k+".nii.gz"
-                tfile = sid + "_strc_FieldMap_Magnitude.nii.gz"
-                tfold = "FieldMap_strc"
-            elif v['name'] == "FM-Phase":
-                sfile = k+".nii.gz"
-                tfile = sid + "_strc_FieldMap_Phase.nii.gz"
-                tfold = "FieldMap_strc"
-            elif "boldref" in v['name']:
-                boldn = v['name'][7:]
-                sfile = k+".nii.gz"
-                tfile = sid + "_fncb_BOLD_"+boldn+"_SBRef.nii.gz"
-                tfold = "BOLD_"+boldn+"_SBRef_fncb"
-                bolds[boldn]["ref"] = sfile
-            elif "bold" in v['name']:
-                boldn = v['name'][4:]
-                sfile = k+".nii.gz"
-                tfile = sid + "_fncb_BOLD_"+boldn+".nii.gz"
-                tfold = "BOLD_"+boldn+"_fncb"
-                bolds[boldn]["bold"] = sfile
-            elif v['name'] == "SE-FM-AP":
-                sfile = k+".nii.gz"
-                tfile = sid + "_fncb_BOLD_AP_SB_SE.nii.gz"
-                tfold = "SpinEchoFieldMap1_fncb"
-            elif v['name'] == "SE-FM-PA":
-                sfile = k+".nii.gz"
-                tfile = sid + "_fncb_BOLD_PA_SB_SE.nii.gz"
-                tfold = "SpinEchoFieldMap1_fncb"
-            elif v['name'] == "DWI":
-                sfile = [k+e for e in ['.nii.gz', '.bval', '.bvec']]
-                tbase = "_".join([sid, 'DWI', v['task']])
-                tfile = [tbase+e for e in ['.nii.gz', '.bval', '.bvec']]
-                tfold = "Diffusion"
-            else:
-                print " ... skipping %s %s" % (v['ima'], v['name'])
+    i = [k for k, v in inf.iteritems() if k.isdigit()]
+    i.sort(key=int, reverse=True)
+
+    for k in i:
+        v = inf[k]
+        if v['name'] == 'T1w':
+            sfile = k+"-o.nii.gz"
+            tfile = sid + "_strc_T1w_MPR1.nii.gz"
+            tfold = "T1w"
+        elif v['name'] == "T2w":
+            sfile = k+"-o.nii.gz"
+            tfile = sid + "_strc_T2w_SPC1.nii.gz"
+            tfold = "T2w"
+        elif v['name'] == "FM-Magnitude":
+            sfile = k+".nii.gz"
+            tfile = sid + "_strc_FieldMap_Magnitude.nii.gz"
+            tfold = "FieldMap_strc"
+        elif v['name'] == "FM-Phase":
+            sfile = k+".nii.gz"
+            tfile = sid + "_strc_FieldMap_Phase.nii.gz"
+            tfold = "FieldMap_strc"
+        elif "boldref" in v['name']:
+            boldn = v['name'][7:]
+            sfile = k+".nii.gz"
+            tfile = sid + "_fncb_BOLD_"+boldn+"_SBRef.nii.gz"
+            tfold = "BOLD_"+boldn+"_SBRef_fncb"
+            bolds[boldn]["ref"] = sfile
+        elif "bold" in v['name']:
+            boldn = v['name'][4:]
+            sfile = k+".nii.gz"
+            tfile = sid + "_fncb_BOLD_"+boldn+".nii.gz"
+            tfold = "BOLD_"+boldn+"_fncb"
+            bolds[boldn]["bold"] = sfile
+        elif v['name'] == "SE-FM-AP":
+            sfile = k+".nii.gz"
+            tfile = sid + "_fncb_BOLD_AP_SB_SE.nii.gz"
+            tfold = "SpinEchoFieldMap"+boldn+"_fncb"
+        elif v['name'] == "SE-FM-PA":
+            sfile = k+".nii.gz"
+            tfile = sid + "_fncb_BOLD_PA_SB_SE.nii.gz"
+            tfold = "SpinEchoFieldMap"+boldn+"_fncb"
+        elif v['name'] == "DWI":
+            sfile = [k+e for e in ['.nii.gz', '.bval', '.bvec']]
+            tbase = "_".join([sid, 'DWI', v['task']])
+            tfile = [tbase+e for e in ['.nii.gz', '.bval', '.bvec']]
+            tfold = "Diffusion"
+        else:
+            print " ... skipping %s %s" % (v['ima'], v['name'])
+            continue
+
+        if type(sfile) is not list:
+            sfile = [sfile]
+        if type(tfile) is not list:
+            tfile = [tfile]
+
+        for sfile, tfile in zip(list(sfile), list(tfile)):
+            if not os.path.exists(os.path.join(rawf, sfile)):
+                print " ---> WARNING: Can not locate %s - skipping the file" % (os.path.join(rawf, sfile))
                 continue
-            
-            if type(sfile) is not list:
-                sfile = [sfile]
-            if type(tfile) is not list:
-                tfile = [tfile]
-            
-            for sfile, tfile in zip(list(sfile), list(tfile)):
-                if not os.path.exists(os.path.join(rawf, sfile)):
-                    print " ---> WARNING: Can not locate %s - skipping the file" % (os.path.join(rawf, sfile))
-                    continue
 
-                if not os.path.exists(os.path.join(basef,tfold)):
-                    print " ---> creating subfolder", tfold
-                    os.makedirs(os.path.join(basef,tfold))
-                else:
-                    print " ...  %s subfolder already exists", tfold
+            if not os.path.exists(os.path.join(basef,tfold)):
+                print " ---> creating subfolder", tfold
+                os.makedirs(os.path.join(basef,tfold))
+            else:
+                print " ...  %s subfolder already exists", tfold
 
-                if not os.path.exists(os.path.join(basef,tfold,tfile)):
-                    print " ---> linking %s to %s" % (sfile, tfile)
-                    os.link(os.path.join(rawf, sfile), os.path.join(basef,tfold,tfile))
-                else:
-                    print " ...  %s already exists" % (tfile)
+            if not os.path.exists(os.path.join(basef,tfold,tfile)):
+                print " ---> linking %s to %s" % (sfile, tfile)
+                os.link(os.path.join(rawf, sfile), os.path.join(basef,tfold,tfile))
+            else:
+                print " ...  %s already exists" % (tfile)
 
     # --- checking if all bolds have refs
 
