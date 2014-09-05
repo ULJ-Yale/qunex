b819b42db1d27efeafa5e4409b3919a35617ebab
Grega Repovs <grega.repovs@psy.ff.uni-lj.si>
2014-09-05 12:27:29 -0400

    Added the option to use covariance when computing correlations.

--- gmri/@gmrimage/mri_ComputeCorrelations.m
+++ gmri/@gmrimage/mri_ComputeCorrelations.m
@@ -1,22 +1,21 @@
-function [correlations, significances] = mri_ComputeCorrelations(obj, bdata, verbose)
+function [correlations, significances] = mri_ComputeCorrelations(obj, bdata, verbose, cv)
 
 %   function [correlations, significances] = mri_ComputeCorrelations(obj, bdata, verbose)
-%	
+%
 %	Computes whole brain GBC based on specified mask and command string
-%	
+%
 %	obj     - image
 %   bdata   - data matrix to compute correlations with
 %   verbose - should it talk a lot [no]
+%   cv      - should it compute covariances instead
 %
 %   Grega Repovš, 2010-03-18
 %
+%   2014-09-03 - added covariance option
 
-if nargin < 3
-    verbose = false;
-    if nargin < 2
-        error('ERROR: No data provided to compute correlations!')
-    end
-end
+if nargin < 4 || isempty(cv),      cv      = false; end
+if nargin < 3 || isempty(verbose), verbose = false; end
+if nargin < 2 error('ERROR: No data provided to compute correlations!'); end
 
 if obj.frames ~= size(bdata,1)
     error('ERROR: data matrix file does not match number of image frames!')
@@ -29,12 +28,23 @@ if verbose, fprintf('\n\nComputing %d correlations with %s', ncorrelations, obj.
 
 if verbose, fprintf('\n... setting up data'), end
 
-if ~obj.correlized    
+if cv
+    if verbose, fprintf('\n... setting up for covariances instead of correlations '), end
+    obj.data = obj.data';
+    obj.data = bsxfun(@minus, obj.data, mean(obj.data)) ./ sqrt(obj.voxels-1);
+    obj.data = obj.data';
+elseif ~obj.correlized && ~cv
     obj = obj.correlize;
 end
 
-bdata = zscore(bdata, 0, 1);
-bdata = bdata ./ sqrt(obj.frames -1);
+if cv
+    bdata = bdata';
+    bdata = bsxfun(@minus, bdata, mean(bdata)) ./ sqrt(obj.voxels-1);
+    bdata = bdata';
+else
+    bdata = zscore(bdata, 0, 1);
+    bdata = bdata ./ sqrt(obj.frames -1);
+end
 
 correlations = zeroframes(obj, ncorrelations);
 
@@ -52,11 +62,15 @@ end
 
 if nargout > 1
     significances = zeroframes(obj, ncorrelations);
-    significances.data = fc_Fisher(correlations.data);
-    significances.data = significances.data/(1/sqrt(obj.frames-3));
+    if cv
+        significances.data(:) = 1;
+    else
+        significances.data = fc_Fisher(correlations.data);
+        significances.data = significances.data/(1/sqrt(obj.frames-3));
+    end
 end
 
 if verbose, fprintf('\n... done!'), end
 
 end
-     
+
