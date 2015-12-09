f4e24d3c120a0a238a67561a280619f3d3e104a5
Grega Repovs <grega.repovs@psy.ff.uni-lj.si>
2015-12-09 22:20:40 +0100

    MaskROI allows masking an existing ROI image with an additional ROI2 image.

---
+++ gmri/@gmrimage/mri_MaskROI.m
@@ -0,0 +1,51 @@
+function [roi] = mri_MaskROI(img, roi2)
+
+%function [img] = mri_MaskROI(img, roi2)
+%
+%		Mask the ROI file based on the second ROI.
+%
+%       img    - the original ROI image
+%       roi2   - the additiona ROI image
+%
+%    (c) Grega Repovs, 2015-12-09
+%
+
+if nargin < 2
+    error('\nERROR: Please provide information on second ROI file to mask the original one!\n');
+end
+
+% ----> Load ROI2 if necessary
+
+roi2 = gmrimage(roi2);
+
+% ----> Process ROI
+
+nroi = length(img.roi.roinames);
+roi  = img.zeroframes(nroi);
+
+for n = 1:nroi
+
+    if length(img.roi.roicodes1{n}) == 0
+        rmask = roi2.mri_ROIMask(img.roi.roicodes2{n});
+    elseif length(img.roi.roicodes2{n}) == 0
+        rmask = img.mri_ROIMask(img.roi.roicodes1{n});
+    else
+        rmask = img.mri_ROIMask(img.roi.roicodes1{n}) & roi2.mri_ROIMask(img.roi.roicodes2{n});
+    end
+
+    roi.data(rmask==1, n) = n;
+    roi.roi.nvox(n) = sum(rmask==1);
+end
+
+% ----> Collapse to a single volume when there is no overlap between ROI
+
+if max(sum(roi.data > 0, 2)) == 1
+    roi.data   = sum(roi.data, 2);
+    roi.frames = 1;
+end
+
+
+% ----> Encode metadata
+
+img.roi.roifile2  = roi2.filename;
+
