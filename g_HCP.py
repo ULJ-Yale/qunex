9bbf843ab6f57d28dec3d6f0ab7febef495dd8bc
Grega Repovš <grega.repovs@mac.com>
2017-02-07 21:04:42 +0100

    Updated documentation for HCP mapping commands.

--- g_HCP.py
+++ g_HCP.py
@@ -1,4 +1,19 @@
-#!/opt/local/bin/python2.7
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+g_HCP.py
+
+Functions for preparing information and mapping images to a HCP preprocessing
+compliant folder structure:
+
+* setupHCP        ... maps the data to a hcp folder
+* setupHCPFolder  ... runs setupHCP for all subject folders
+* getHCPReady     ... prepares subject.txt files for HCP mapping
+
+The commands are accessible from the terminal wusing gmri utility.
+
+Copyright (c) Grega Repovs. All rights reserved.
+"""
 
 import os
 import glob
@@ -12,11 +27,80 @@ def setupHCP(folder=".", tfolder="hcp", sbjf="subject_hcp.txt"):
     '''
     setupHCP [folder=.] [tfolder=hcp] [sbjf=subject_hcp.txt]
 
-    - folder:  the base subject folder that contains the nifti images and subject.txt file
-    - tfolder: the folder (within the base folder) where to put the HCP data
-    - sbjf:    the alternative name of the subject.txt file
+    USE
+    ===
 
-    example: gmri setupHCP folder=data tfolder=hcp2 sbjf=subject1.txt
+    The command maps images from the subject's nii folder into a folder
+    structure that conforms to the naming conventions used in the HCP
+    minimal preprocessing workflow. For the mapping to be correct, the
+    command expects the source subject.txt file (sbjf) to hold the relevant
+    information on images. To save space, the images are not copied into the new
+    folder structure but rather hard-links are created if possible.
+
+    PARAMETERS
+    ==========
+
+    --folder   The base subject folder that contains the nifti images and
+               subject.txt file. [.]
+    --tfolder  The folder (within the base folder) to which the data is to be
+               mapped. [hcp]
+    --sbjf     The name of the source subject.txt file. [subject_hcp.txt]
+
+    IMAGE DEFINITION
+    ================
+
+    For the mapping to work, each MR to be mapped has to be marked with the
+    appropriate image type in the source.txt file. The following file types
+    are recognized and will be mapped correctly:
+
+    T1w            ... T1 weighted high resolution structural image
+    T2w            ... T2 weighted high resolution structural image
+    FM-Magnitude   ... Field mapping magnitude image used for distortion
+                       correction
+    FM-Phase       ... Field mapping phase image used for distortion
+                       correction
+    boldref[N]     ... Reference image for the following BOLD image
+    bold[N]        ... BOLD image
+    SE-FM-AP       ... Spin-echo fieldmap image recorded using the A-to-P
+                       frequency readout direction
+    SE-FM-PA       ... Spin-echo fieldmap image recorded using the P-to-A
+                       frequency readout direction
+    SE-FM-LR       ... Spin-echo fieldmap image recorded using the L-to-R
+                       frequency readout direction
+    SE-FM-RL       ... Spin-echo fieldmap image recorded using the R-to-L
+                       frequency readout direction
+    DWI            ... Diffusion weighted image
+
+    Example definition
+    ------------------
+
+    hcpready: true
+    01:                 :Survey
+    02: T1w             :T1w 0.7mm N1
+    03: T2w             :T2w 0.7mm N1
+    04:                 :Survey
+    05: SE-FM-AP        :C-BOLD 3mm 48 2.5s FS-P
+    06: SE-FM-PA        :C-BOLD 3mm 48 2.5s FS-A
+    07: bold1:WM        :BOLD 3mm 48 2.5s
+    08: bold2:WM        :BOLD 3mm 48 2.5s
+    09: bold3:WM        :BOLD 3mm 48 2.5s
+    10: bold4:WM        :BOLD 3mm 48 2.5s
+    11: bold5:WM        :BOLD 3mm 48 2.5s
+    12: bold6:WM        :BOLD 3mm 48 2.5s
+    13: bold7:rest      :RSBOLD 3mm 48 2.5s
+    14: bold8:rest      :RSBOLD 3mm 48 2.5s
+
+    EXAMPLE USE
+    ===========
+
+    gmri setupHCP folder=OP316 sbjf=subject.txt
+
+    ----------------
+    Written by Grega Repovš
+
+    Changelog
+    2017-02-07 Grega Repovš
+             - Updated documentation
     '''
 
     inf = niutilities.g_core.readSubjectData(os.path.join(folder, sbjf))[0][0]
@@ -47,59 +131,59 @@ def setupHCP(folder=".", tfolder="hcp", sbjf="subject_hcp.txt"):
     for k in i:
         v = inf[k]
         if 'o' in v:
-            orient = "_"+v['o']
+            orient = "_" + v['o']
         else:
             orient = ""
         if v['name'] == 'T1w':
             nT1w += 1
-            sfile = k+"-o.nii.gz"
+            sfile = k + "-o.nii.gz"
             tfile = sid + "_strc_T1w_MPR%d.nii.gz" % (nT1w)
             tfold = "T1w"
         elif v['name'] == "T2w":
             nT2w += 1
-            sfile = k+"-o.nii.gz"
+            sfile = k + "-o.nii.gz"
             tfile = sid + "_strc_T2w_SPC%d.nii.gz" % (nT2w)
             tfold = "T2w"
         elif v['name'] == "FM-Magnitude":
-            sfile = k+".nii.gz"
+            sfile = k + ".nii.gz"
             tfile = sid + "_strc_FieldMap_Magnitude.nii.gz"
             tfold = "FieldMap_strc"
         elif v['name'] == "FM-Phase":
-            sfile = k+".nii.gz"
+            sfile = k + ".nii.gz"
             tfile = sid + "_strc_FieldMap_Phase.nii.gz"
             tfold = "FieldMap_strc"
         elif "boldref" in v['name']:
             boldn = v['name'][7:]
-            sfile = k+".nii.gz"
-            tfile = sid + "_fncb_BOLD_"+boldn+orient+"_SBRef.nii.gz"
-            tfold = "BOLD_"+boldn+orient+"_SBRef_fncb"
+            sfile = k + ".nii.gz"
+            tfile = sid + "_fncb_BOLD_" + boldn + orient + "_SBRef.nii.gz"
+            tfold = "BOLD_" + boldn + orient + "_SBRef_fncb"
             bolds[boldn]["ref"] = sfile
         elif "bold" in v['name']:
             boldn = v['name'][4:]
-            sfile = k+".nii.gz"
-            tfile = sid + "_fncb_BOLD_"+boldn+orient+".nii.gz"
-            tfold = "BOLD_"+boldn+orient+"_fncb"
+            sfile = k + ".nii.gz"
+            tfile = sid + "_fncb_BOLD_" + boldn + orient + ".nii.gz"
+            tfold = "BOLD_" + boldn + orient + "_fncb"
             bolds[boldn]["bold"] = sfile
         elif v['name'] == "SE-FM-AP":
-            sfile = k+".nii.gz"
+            sfile = k + ".nii.gz"
             tfile = sid + "_fncb_BOLD_AP_SB_SE.nii.gz"
-            tfold = "SpinEchoFieldMap"+boldn+"_fncb"
+            tfold = "SpinEchoFieldMap" + boldn + "_fncb"
         elif v['name'] == "SE-FM-PA":
-            sfile = k+".nii.gz"
+            sfile = k + ".nii.gz"
             tfile = sid + "_fncb_BOLD_PA_SB_SE.nii.gz"
-            tfold = "SpinEchoFieldMap"+boldn+"_fncb"
+            tfold = "SpinEchoFieldMap" + boldn + "_fncb"
         elif v['name'] == "SE-FM-LR":
-            sfile = k+".nii.gz"
+            sfile = k + ".nii.gz"
             tfile = sid + "_fncb_BOLD_LR_SB_SE.nii.gz"
-            tfold = "SpinEchoFieldMap"+boldn+"_fncb"
+            tfold = "SpinEchoFieldMap" + boldn + "_fncb"
         elif v['name'] == "SE-FM-RL":
-            sfile = k+".nii.gz"
+            sfile = k + ".nii.gz"
             tfile = sid + "_fncb_BOLD_RL_SB_SE.nii.gz"
-            tfold = "SpinEchoFieldMap"+boldn+"_fncb"
+            tfold = "SpinEchoFieldMap" + boldn + "_fncb"
         elif v['name'] == "DWI":
-            sfile = [k+e for e in ['.nii.gz', '.bval', '.bvec']]
+            sfile = [k + e for e in ['.nii.gz', '.bval', '.bvec']]
             tbase = "_".join([sid, 'DWI', v['task']])
-            tfile = [tbase+e for e in ['.nii.gz', '.bval', '.bvec']]
+            tfile = [tbase + e for e in ['.nii.gz', '.bval', '.bvec']]
             tfold = "Diffusion"
         else:
             print " ... skipping %s %s" % (v['ima'], v['name'])
@@ -115,15 +199,15 @@ def setupHCP(folder=".", tfolder="hcp", sbjf="subject_hcp.txt"):
                 print " ---> WARNING: Can not locate %s - skipping the file" % (os.path.join(rawf, sfile))
                 continue
 
-            if not os.path.exists(os.path.join(basef,tfold)):
+            if not os.path.exists(os.path.join(basef, tfold)):
                 print " ---> creating subfolder", tfold
-                os.makedirs(os.path.join(basef,tfold))
+                os.makedirs(os.path.join(basef, tfold))
             else:
                 print " ...  %s subfolder already exists", (tfold)
 
-            if not os.path.exists(os.path.join(basef,tfold,tfile)):
+            if not os.path.exists(os.path.join(basef, tfold, tfile)):
                 print " ---> linking %s to %s" % (sfile, tfile)
-                os.link(os.path.join(rawf, sfile), os.path.join(basef,tfold,tfile))
+                os.link(os.path.join(rawf, sfile), os.path.join(basef, tfold, tfile))
             else:
                 print " ---> %s already exists" % (tfile)
                 # print " ---> %s already exists, replacing it with %s " % (tfile, sfile)
@@ -165,17 +249,49 @@ def setupHCPFolder(folder=".", tfolder="hcp", sbjf="subject_hcp.txt", check="int
     '''
     setupHCPFolder [folder=.] [tfolder=hcp] [sbjf=subject_hcp.txt] [check=interactive]
 
-    The command looks for sbjf files in all the subfolders of folder and check whether the sbjf are hcp ready
-    and if tfolder exists. If the file is ready and folder does not yet exist, it automatically calls setupHCP
-    on that folder. If the sbjf does not seem to be ready or if the tfolder exists, the action depends on check.
-    If check is "yes", the subject is not processed, if check is "no" the subject is processed. If check is
-    "interactive" the user is asked whether the subject should be processed or not.
+    USE
+    ===
 
-    - folder:  the directory that holds the subjects' folders (usually "subjects")
-    - tfolder: the folder in which to set up data for HCP preprocessing
-    - sbjf:    the subject.txt file to use for mapping to HCP folder
-    - check:   whether to check if the subject is safe to run (yes), run in in any case (no) or
-               ask the user (interactive)
+    The command is used to map MR images into a HCP prepocessing folder
+    structure for all the subject folders it finds within the specified
+    origin folder (folder).
+
+    Specifically, the command looks for source subject.txt files (sbjf) in all
+    the subfolders of the origin folder (folder). For each found source
+    subject.txt file it checks whether the file is hcp ready and if the target
+    folder (tfolder) exists. If the file is ready and if the target folder
+    does not yet exists, it runs setupHCP command mapping the files to the
+    target folder as specified in the source subject.txt file.
+
+    If the source subject.txt file does not seem to be ready or if the target
+    folder exists, the action depends on check parameter. If check is "yes",
+    the subject is not processed, if check is set to "no" the subject is
+    processed. If check is set to "interactive" the user is asked whether the
+    subject should be processed or not.
+
+    PARAMETERS
+    ==========
+
+    --folder   The origin folder that holds the subjects' folders (usually
+               "subjects"). [.]
+    --tfolder  The target HCP folder in which to set up data for HCP
+               preprocessing (usually "hcp"). [hcp]
+    --sbjf     The source subject.txt file to use for mapping to a target HCP
+               folder. [subject_hcp.txt]
+    --check    Whether to check if the subject is safe to run (yes), run in any
+               case (no) or ask the user (interactive) if in doubt.
+
+    EXAMPLE USE
+    ===========
+
+    gmri setupHCPFolder folder=subjects check=no
+
+    ----------------
+    Written by Grega Repovš
+
+    Changelog
+    2017-02-07 Grega Repovš
+             - Updated documentation.
     '''
 
     # list all possible sbjfiles and check them
@@ -241,21 +357,69 @@ def getHCPReady(folder=".", sfile="subject.txt", tfile="subject_hcp.txt", patter
     '''
     getHCPReady [folder=.] [sfile=subject.txt] [tfile=subject_hcp.txt] [pattern="*"] [mapping=specs/hcpmap.txt]
 
-    The command checks all the directories in the folder that match the pattern checking for the presence of sfile.
-    If sfile is found, each sequence name is checked against the source specified in hcpmap.txt file and replaced
-    with the provided text. The resulting file is saved to tfile. In hcpmap.txt the mapping is specified line by line
-    in the form of "source text => replacement text". All lines not matching the pattern are ignored.
+    USE
+    ===
 
-    - folder:   the directory that holds subjects' folders (usually "subjects")
-    - sfile:    the "source" subject.txt file
-    - tfile:    the replacement subject.txt file
-    - pattern:  glob pattern to use in identifying subject folders
-    - mapping:  the path to the text file describing the mapping
+    The command is used to prepare subject.txt files so that they hold the
+    information necessary for correct mapping to a fodler structure supporting
+    HCP preprocessing.
+
+    The command checks all the directories in the folder that match the pattern,
+    looking for the presence of specified source file (sfile). If the source
+    file is found, each sequence name is checked against the source specified in
+    the mapping file (mapping), and replaced with the provided text. The results
+    are then saved to the specified target file (tfile). The resulting files will
+    have "hcpready: true" key-value pair added.
+
+    PARAMETERS
+    ==========
+
+    --folder   the directory that holds subjects' folders (usually "subjects") [.]
+    --sfile    the "source" subject.txt file [subject.txt]
+    --tfile    the "target' subject.txt file [subject_hcp.txt]
+    --pattern  glob pattern to use in identifying subject folders ["*"]
+    --mapping  the path to the text file describing the mapping [specs/hcpmap.txt]
+
+    Mapping specification
+    ---------------------
+
+    The mapping file specifies what source text is to be replaced with what
+    target text. There are no limits to the number of mappings. Mappings that
+    are not found in the source file will not be used. All lines not matching
+    the pattern are ignored.Each mapping is to be specified in a single line in
+    a simple form:
+
+    "<source text> => <replacement text>"
+
+    Example lines in a mapping file:
+
+    C-BOLD 3mm 48 2.5s FS-P => SE-FM-AP
+    C-BOLD 3mm 48 2.5s FS-A => SE-FM-PA
+
+    T1w 0.7mm N1 => T1w
+    T1w 0.7mm N2 => T1w
+    T2w 0.7mm N1 => T2w
+    T2w 0.7mm N2 => T2w
+
+    RSBOLD 3mm 48 2.5s  => bold:rest
+    BOLD 3mm 48 2.5s    => bold:WM
+
+    EXAMPLE USE
+    ===========
+
+    gmri getHCPReady folder=subjects pattern="OP*" mapping=subjects/maphcp.txt
+
+    ----------------
+    Written by Grega Repovš
+
+    Changelog
+    2017-02-07 Grega Repovš
+             - Updated documentation.
     '''
 
-    if pattern == None:
+    if pattern is None:
         pattern = "*"
-    if mapping == None:
+    if mapping is None:
         mapping = os.path.join(folder, 'specs', 'hcpmap.txt')
 
     # -- get mapping ready
