2732e56e9e6562ac13a00e4ecef3660f14bc38c2
Alan Anticevic of group anticevic <aa353@grace2.grace.hpc.yale.internal>
2018-01-16 14:58:50 -0500

    --Update--via-grace2.grace.hpc.yale.internal-2018-01-16-14-58-49
    Former-commit-id: 2d966f88e9d1fe26783fa46e082a024122cf2ac0

--- mnap.sh
+++ mnap.sh
@@ -3763,13 +3763,16 @@ if [[ "$setflag" =~ .*-.* ]]; then
 	## -- If subjects folder is missing but study folder is defined assume standard MNAP folder structure	
 	if [ -z "$SubjectsFolder" ]; then
 		if [ -z "$StudyFolder" ]; then
+		return 0
 		else
 			SubjectsFolder="$StudyFolder/subjects"
 		fi
 	fi
+	
 	## -- If study folder is missing but subjects folder is defined assume standard MNAP folder structure	
 	if [ -z "$StudyFolder" ]; then
 		if [ -z "$SubjectsFolder" ]; then
+		return 0
 		else
 			cd $SubjectsFolder/../ &> /dev/null
 			StudyFolder=`pwd` &> /dev/null
