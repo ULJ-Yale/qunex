aebc340a539ca1ea1fa2f9437f8a11b5674503e5
Grega Repovš <grega.repovs@mac.com>
2018-07-26 17:09:33 +0200

    Prepared (hopefully) octave compatible error reporting.

--- general/g_ReportError.m
+++ general/g_ReportError.m
@@ -8,4 +8,19 @@ function [] = g_ReportError(ME);
 %   Written by Grega Repovš on 2018-06-20.
 %
 
-fprintf('\n\n=========================================\nExecution error! Processing failed! \nPlease check arguments and/or try running the command in Matlab or Octave directly.\n\nThe exact error reported:\n-----------------------------------------\n%s\n=========================================\n', getReport(ME))
+fprintf('\n\n=========================================\nExecution error! Processing failed! \nPlease check arguments and/or try running the command in Matlab or Octave directly.\n\nThe exact error reported:\n-----------------------------------------\n%s\n\n=========================================\n', prepareErrorReport(ME))
+
+
+
+function [s] = prepareErrorReport(ME);
+
+    s = '';
+    s = [s sprintf('\nError identifier: %s', ME.identifier)];
+    s = [s sprintf('\n   Error message: %s', ME.message)];
+    for n = 1:length(ME.stack)
+        if n == 1
+            s = [s sprintf('\n     Error stack: %s -> %s [line: %d]', ME.stack(n).file, ME.stack(n).name, ME.stack(n).line)];
+        else
+            s = [s sprintf('\n                  %s -> %s [line: %d]', ME.stack(n).file, ME.stack(n).name, ME.stack(n).line)];
+        end
+    end
