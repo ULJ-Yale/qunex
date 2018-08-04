1139888f232fbb49fffafd9abb78d4da12f2402d
Grega Repovš <grega.repovs@mac.com>
2018-08-04 11:35:25 +0200

    Updated CommandError definition.

--- g_exceptions.py
+++ g_exceptions.py
@@ -9,7 +9,18 @@ Definition of exceptions used in gmri
 
 class CommandError(Exception):
     """There was an error in calling the command."""
-
+    
+    def __init__(self, function=None, error=None, *hints):
+        if function is None:
+            function = "unknown function"
+        if error is None:
+            error = "unspecified"
+        msg = "Error '%s' occured in %s" % (error, function)
+        super(CommandFailed, self).__init__(msg)
+        self.function = function
+        self.error    = error
+        self.hints    = hints
+        self.report   = (error,) + hints
 
 class CommandFailed(Exception):
     """A command has failed to carry out fully."""
