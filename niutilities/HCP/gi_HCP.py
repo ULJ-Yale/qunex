3674ec0e74adf81e9673ad3c404ee2aa2fd4acae
Grega Repovs [at Grace] <grega.repovs@yale.edu>
2020-08-22 08:42:50 -0400

    feat: add LR/RL and i-/i to phase encoding direction mapping
    Only AP/PA to j-/j mapping was specified in the relevant HCP processing
    code. LR/RL to i-/i mapping is now added.

--- niutilities/HCP/gi_HCP.py
+++ niutilities/HCP/gi_HCP.py
@@ -29,7 +29,7 @@ import ast
 
 unwarp = {None: "Unknown", 'i': 'x', 'j': 'y', 'k': 'z', 'i-': 'x-', 'j-': 'y-', 'k-': 'z-'}
 PEDir  = {None: "Unknown", "LR": 1, "RL": 1, "AP": 2, "PA": 2}
-PEDirMap  = {'AP': 'j-', 'j-': 'AP', 'PA': 'j', 'j': 'PA'}
+PEDirMap  = {'AP': 'j-', 'j-': 'AP', 'PA': 'j', 'j': 'PA', 'RL': 'i', 'i': 'RL', 'LR': 'i-', 'i-': 'LR'}
 
 
 def mapToQUNEXcpls(file, sessionsfolder, hcplsname, sessions, overwrite, prefix, nameformat):
