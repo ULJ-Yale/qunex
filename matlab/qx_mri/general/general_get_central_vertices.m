636c34c2ece5cb0fc72d0586f2ae39db87ea30bf
Aleksij <alex.kraljic@gmail.com>
2021-05-20 10:22:15 +0200

    fix: added an if statement to check if matlab or octave

--- matlab/qx_mri/general/general_get_central_vertices.m
+++ matlab/qx_mri/general/general_get_central_vertices.m
@@ -20,7 +20,7 @@ function central_vertices = general_get_central_vertices(fin, fout, projection)
 %   --central_vertices   An array of structs with information about the
 %                        most central region vertices. The output is
 %                        structured as ::
-%                        
+%
 %                        `central_vertices(<frame #>).(<surface structure>).{<region id>,:}`
 %
 %                        The first element corresponds to the region label
@@ -40,71 +40,79 @@ function central_vertices = general_get_central_vertices(fin, fout, projection)
 if nargin < 3 || isempty(projection), projection = 'midthickness'; end
 if nargin < 2 || isempty(fout),       fout       = '';             end
 
-load('cifti_brainmodel');
+is_octave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 
-params.cifti = cifti;
-params.components = components;
-params.projection = projection;
-params.weigh_by_distance = true;
-
-if ischar(fin)
-    img = nimage(fin);
-else
-    img = fin;
-end
-
-for fr=1:img.frames
-    for cmp=1:2
-        params.surfaceComponent = params.cifti.shortnames{cmp};
-        params.cmp = cmp;
-        
-        indexedData = zeros(32492,1);
-        D = img.data(img.cifti.start(params.cmp):img.cifti.end(params.cmp),fr);
-        indexedData(params.cifti.(params.surfaceComponent).mask) = D;
-        
-        regions = unique(indexedData(indexedData ~= 0));
-        n_regions = length(regions);
-        
-        central_vertices(fr).(params.cifti.shortnames{cmp}) = cell(n_regions,3);
-        
-        for r=1:n_regions
-            [central_idx, central_xyz] = get_most_central_vertex(params, indexedData, regions(r));
-            central_vertices(fr).(params.surfaceComponent){r,1} = regions(r);
-            central_vertices(fr).(params.surfaceComponent){r,2} = central_idx;
-            central_vertices(fr).(params.surfaceComponent){r,3} = central_xyz;
-        end
-    end
-end
-
-if ~isempty(fout)
-    repf = fopen(fout, 'w');
+if ~is_octave
     
-    fprintf(repf, '#source: %s', img.filename);
-    fprintf(repf, '\n#projection: %s', params.projection);
+    load('cifti_brainmodel');
+    
+    params.cifti = cifti;
+    params.components = components;
+    params.projection = projection;
+    params.weigh_by_distance = true;
+    
+    if ischar(fin)
+        img = nimage(fin);
+    else
+        img = fin;
+    end
     
     for fr=1:img.frames
-        if img.frames > 1
-            fprintf(repf, '\n\nFrame #%d:\n', fr);
-        else
-            fprintf(repf, '\n');
-        end
-        
-        fprintf(repf, '\nRegion\tCentral_index\tCentral_x\tCentral_y\tCentral_z');
-        
         for cmp=1:2
-            n_regions = size(central_vertices(fr).(params.cifti.shortnames{cmp}),1);
+            params.surfaceComponent = params.cifti.shortnames{cmp};
+            params.cmp = cmp;
+            
+            indexedData = zeros(32492,1);
+            D = img.data(img.cifti.start(params.cmp):img.cifti.end(params.cmp),fr);
+            indexedData(params.cifti.(params.surfaceComponent).mask) = D;
+            
+            regions = unique(indexedData(indexedData ~= 0));
+            n_regions = length(regions);
+            
+            central_vertices(fr).(params.cifti.shortnames{cmp}) = cell(n_regions,3);
+            
             for r=1:n_regions
-                fprintf(repf, '\n%d\t%0.3f\t%0.3f\t%0.3f\t%0.3f',...
-                    central_vertices(fr).(params.cifti.shortnames{cmp}){r,1},...
-                    central_vertices(fr).(params.cifti.shortnames{cmp}){r,2},...
-                    central_vertices(fr).(params.cifti.shortnames{cmp}){r,3}(1),...
-                    central_vertices(fr).(params.cifti.shortnames{cmp}){r,3}(2),...
-                    central_vertices(fr).(params.cifti.shortnames{cmp}){r,3}(3));
+                [central_idx, central_xyz] = get_most_central_vertex(params, indexedData, regions(r));
+                central_vertices(fr).(params.surfaceComponent){r,1} = regions(r);
+                central_vertices(fr).(params.surfaceComponent){r,2} = central_idx;
+                central_vertices(fr).(params.surfaceComponent){r,3} = central_xyz;
             end
         end
     end
     
-    fclose(repf);
+    if ~isempty(fout)
+        repf = fopen(fout, 'w');
+        
+        fprintf(repf, '#source: %s', img.filename);
+        fprintf(repf, '\n#projection: %s', params.projection);
+        
+        for fr=1:img.frames
+            if img.frames > 1
+                fprintf(repf, '\n\nFrame #%d:\n', fr);
+            else
+                fprintf(repf, '\n');
+            end
+            
+            fprintf(repf, '\nRegion\tCentral_index\tCentral_x\tCentral_y\tCentral_z');
+            
+            for cmp=1:2
+                n_regions = size(central_vertices(fr).(params.cifti.shortnames{cmp}),1);
+                for r=1:n_regions
+                    fprintf(repf, '\n%d\t%0.3f\t%0.3f\t%0.3f\t%0.3f',...
+                        central_vertices(fr).(params.cifti.shortnames{cmp}){r,1},...
+                        central_vertices(fr).(params.cifti.shortnames{cmp}){r,2},...
+                        central_vertices(fr).(params.cifti.shortnames{cmp}){r,3}(1),...
+                        central_vertices(fr).(params.cifti.shortnames{cmp}){r,3}(2),...
+                        central_vertices(fr).(params.cifti.shortnames{cmp}){r,3}(3));
+                end
+            end
+        end
+        
+        fclose(repf);
+    end
+    
+else
+    fprintf('\n---> ERROR: Function general_get_central_vertices is currently not supported when running Qu|Nex with Octave - EXITING! \n');
 end
 
 end
@@ -147,5 +155,6 @@ C = centrality(G,'betweenness');
 
 [~, central_idx] = max(C);
 central_xyz = xyz(central_idx,:);
+
 
 end
