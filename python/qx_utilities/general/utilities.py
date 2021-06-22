087780795a988272d7aa349ba5266c34bac640a3
Jure Demsar <demsarjure@gmail.com>
2021-06-22 02:00:17 -0400

    fix: run_list package imports

--- python/qx_utilities/general/utilities.py
+++ python/qx_utilities/general/utilities.py
@@ -25,6 +25,8 @@ import glob
 import getpass
 import re
 import subprocess
+import commands as gcom
+import commands_support as gcs
 import process as gp
 import core as gc
 import processing.core as gpc
@@ -1817,10 +1819,10 @@ def run_list(listfile=None, runlists=None, logfolder=None, verbose="no", eargs=N
 
             # -- remove parameters that are not allowed
 
-            if commandName in commands.commands:
-                allowedParameters = list(commands.commands.get(commandName)["args"])
+            if commandName in gcom.commands:
+                allowedParameters = list(gcom.commands.get(commandName)["args"])
                 if any([e in allowedParameters for e in ['sourcefolder', 'folder']]):
-                    allowedParameters += commands_support.extra_parameters
+                    allowedParameters += gcs.extra_parameters
                 for param in commandParameters.keys():
                     if param not in allowedParameters:
                         del commandParameters[param]
