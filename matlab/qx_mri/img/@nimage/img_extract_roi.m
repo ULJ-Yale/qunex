416ca874c4f24c3c8648660779e592ec6631340f
Grega Repovs @kanga <grega.repovs@mac.com>
2023-08-09 15:58:34 +0000

    chore: minor changes and optimizations before refactoring roi procesing

--- matlab/qx_mri/img/@nimage/img_extract_roi.m
+++ matlab/qx_mri/img/@nimage/img_extract_roi.m
@@ -14,7 +14,7 @@ function ts = img_extract_roi(obj, roi, rcodes, method, weights, criterium)
 %   --method      method name [mean]
 %
 %                 - 'mean'      ... average value of the ROI
-%                  - 'median'    ... median value across the ROI
+%                 - 'median'    ... median value across the ROI
 %                 - 'pca'       ... first eigenvariate of the ROI
 %                 - 'threshold' ... average of all voxels above threshold
 %                 - 'maxn'      ... average of highest n voxels
