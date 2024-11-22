7623f2e49eb584c223cfb517719a505b252ae0a3
Jure Demsar <demsarjure@gmail.com>
2024-11-22 17:40:22 +0000

    fix: fixed a bug where --test did not work in some cases

--- python/qx_utilities/hcp/process_hcp.py
+++ python/qx_utilities/hcp/process_hcp.py
@@ -7031,7 +7031,7 @@ def executeHCPMultiICAFix(sinfo, options, overwrite, hcp, run, group):
                     r,
                     overwrite=overwrite,
                 )
-                if passed is None:
+                if passed is "done":
                     r += "\n---> multi-run HCP ICAFix can be run"
                     report["ready"].append(groupname)
                 else:
