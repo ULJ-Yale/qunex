66cc8775c50e19a36ea4e75bb23e8b9addaf8c52
samuelbrege <sabrege@gmail.com>
2026-04-15 14:29:02 -0400

    chore: removed unnecessary print

--- python/qx_utilities/hcp/process_hcp.py
+++ python/qx_utilities/hcp/process_hcp.py
@@ -326,7 +326,6 @@ def getHCPPaths(sinfo, options):
     if len(t1w_afi) != 0:
         d["T1w-AFI"] = t1w_afi[0]
     else:
-        print(os.path.join(d["T1w_source"], sinfo["id"] + "*_AFI.nii.gz"))
         t1w_afi = glob.glob(os.path.join(d["T1w_source"], sinfo["id"] + "*AFI.nii.gz"))
         if len(t1w_afi) != 0:
             d["T1w-AFI"] = t1w_afi[0]
